a5549bed9dfb47d7f4a4cb1df81d11e920a1bbe6
EC2 Default User <ec2-user@ip-172-31-46-141.us-east-2.compute.internal>
2020-08-21 19:37:45 +0000

    Added Dialog to ammo component. Data not FULLY bound back to AmmoComponent yet

--- src/app/ammo/ammo.component.ts
+++ src/app/ammo/ammo.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AmmoTicket } from '../../assets/interfaces/ammo-ticket';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/option-dialog.component';
+
+export interface DialogData {
+  name: string;
+  option: string;
+}
 
 @Component({
   selector: 'app-ammo',
@@ -8,26 +15,43 @@ import { AmmoTicket } from '../../assets/interfaces/ammo-ticket';
   styleUrls: ['./ammo.component.css']
 })
 export class AmmoComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router, public dialog: MatDialog) { }
+  
   reload: boolean;
   ticket: AmmoTicket;
   ammoTickets: AmmoTicket[];
-  selectOpt: boolean;
   
   ngOnInit(): void {
     if (this.router.url == '/reload'){
       console.log(this.router.url);
       this.reload = true;
+      this.ticket = {
+        cal: '',
+        bulletWeight: 0,
+        bulletType: '',
+        powderWeight: 0,
+        powderBrand: '',
+        primerBrand: '',
+        rounds: 0
+      }
     }
     else{
       console.log(this.router.url);
       this.reload = false;
+      this.ticket = {
+        cal: '',
+        brand: '',
+        bulletWeight: 0,
+        bulletType: '',
+        rounds: 0
+      }
     }
   }
   
-  updateTicket(ticket, info){
-    ticket = info;
-    console.log(ticket);
+  updateTicket(/*ticket, info*/){
+    // ticket = info;
+    // console.log(ticket);
+    console.log("Function is functioning lol")
   }
   
   saveTicket(){
@@ -35,13 +59,25 @@ export class AmmoComponent implements OnInit {
     //this.ammoTickets.push(this.ticket);
   }
   
-  addOption(boo: boolean){
-    if(boo) 
-      this.selectOpt = boo;
-    else if(!boo){
-      //new comlumn/row in 
-      this.selectOpt = boo;
-    }
+  addOption(name: string){
+    let data;
+    Object.keys(this.ticket).forEach(item => {
+      if (name == item){
+        console.log(item)
+        if (!this.ticket[item]) 
+          this.ticket[item] = ' ';
+        data = this.ticket[item]
+      }
+    })
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: {name: name, option: data}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed. Ticket: ' + this.ticket);
+      
+    });
   }
   
   sendTickets(){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+ 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 import { AmmoComponent } from './ammo/ammo.component';
 import { LogComponent } from './log/log.component';
-import { AccountComponent } from './account/account.component'
+import { AccountComponent } from './account/account.component';
+import { DialogComponent } from './dialog/option-dialog.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,8 @@ import { AccountComponent } from './account/account.component'
     HomeComponent,
     AmmoComponent,
     LogComponent,
-    AccountComponent
+    AccountComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
@@ -34,9 +39,12 @@ import { AccountComponent } from './account/account.component'
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule
+    MatTableModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DialogComponent]
 })
 export class AppModule { }
